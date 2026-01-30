b5e6e849da51e27f172e4d2b91257e04805251f7
Gustavo Balaguera <gustavobalaguera214@gmail.com>
2026-01-30 17:07:02 -0500

    Resolved merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Awards from './components/Awards';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Blog from './components/Blog';
-import BlogPost from './components/BlogPost';
-import ScrollToTop from './components/ScrollToTop';
-import Demo from './components/ui/demo';
-=======
+
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -21,7 +7,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
->>>>>>> 2dcda7d (Replaced old version with new updated version)
 
 const queryClient = new QueryClient();
 
@@ -32,26 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
-          <Route path="/" element={
-            <main>
-              <Hero />
-              <About />
-              <Experience />
-              <Projects />
-              <Skills />
-              <Awards />
-              <Contact />
-            </main>
-          } />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
-          <Route path="/starfall" element={<Demo />} />
-=======
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
->>>>>>> 2dcda7d (Replaced old version with new updated version)
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
